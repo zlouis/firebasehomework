0cfa8cb54fcbb1823df9ce5207d59ae690ff5c92
zlouis <jiannchyao@gmail.com>
2016-09-19 20:30:32 -0400

    First Draft

---
+++ assets/javascript/game.js
@@ -0,0 +1,9 @@
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyB6fMAP1ES7G21doGxcxKVRAJDuEjcesOE",
+    authDomain: "trainschedule-a7cd2.firebaseapp.com",
+    databaseURL: "https://trainschedule-a7cd2.firebaseio.com",
+    storageBucket: "",
+    messagingSenderId: "652497974428"
+  };
